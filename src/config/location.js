0f8983ba3f1bf9aa5328812d6c1b165eb0331a6a
ratherforky <jf16688@bristol.ac.uk>
2024-03-28 18:08:17 +0000

    replaced broken map and ran `npm audi fix`

--- src/config/location.js
+++ src/config/location.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HorizontalComponent from "../components/generics/horizontalComponent";
+import ImageComponent from "../components/generics/imageComponent";
 
 export const createLocation = () => {
   return [
@@ -9,30 +10,27 @@ export const createLocation = () => {
         <div>
           <h2 className="mb-4">Location</h2>
           <p>
-            We are a research group in the School of Computer Science,
-            which is part of the Faculty of Engineering.
+            We are a research group in the School of Computer Science, which is
+            part of the Faculty of Engineering.
           </p>
           <div className="primary-font">
             <p>
-               Programming Languages Research Group<br></br>
-               Merchant Venturers Building<br></br>
-               Woodland Road<br></br>
-               BS8 1UB<br></br>
-               Bristol<br></br>
-               United Kingdom<br></br>
+              Programming Languages Research Group<br></br>
+              Merchant Venturers Building<br></br>
+              Woodland Road<br></br>
+              BS8 1UB<br></br>
+              Bristol<br></br>
+              United Kingdom<br></br>
             </p>
           </div>
         </div>,
-        <iframe
-          title="Map Location"
-          width="100%"
-          height="400px"
-          style={{ border: 0 }}
-          loading="lazy"
-          allowFullScreen
-          referrerPolicy="no-referrer-when-downgrade"
-          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBYUK4Cnb3Bg9ZC8HR-J8mZv91QY4oLAkQ&q=Merchant+Venturers+Building,Woodland+Rd,Bristol,BS8+1UB,United Kingdom"
-        ></iframe>,
+        <ImageComponent
+          alt={
+            "Map showing the location of the Merchant Venturers (a pox upon them) Building"
+          }
+          src={"images/mvb-map.png"}
+          maxHeight="400px"
+        />,
       ]}
     />,
   ];
