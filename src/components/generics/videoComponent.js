b7241056d5170a2138d3cebecd0c287066033399
ratherforky <jf16688@bristol.ac.uk>
2026-09-09 19:22:20 +0100

    makes youtube embeds better formatted on mobile + fix low quality thumbnail issue

--- src/components/generics/videoComponent.js
+++ src/components/generics/videoComponent.js
@@ -5,28 +5,29 @@ import Spinner from "../spinner";
 
 // NOTE: FITS CONTAINER
 // Currently only tested to work with Youtube embeds.
-const Video = ({ src, caption = null, width = 560, height = 315 }) => {
+const Video = ({ src, caption = null }) => {
   const [loaded, setLoaded] = useState(false);
-
-  // Make sure never gets too big if the container overflows page:
-  const frameStyle = { maxHeight: "90vh", maxWidth: "90vw" };
 
   return (
     <div className="d-flex justify-content-center my-2">
       {/* If container overflowing, make sure content is still centralised not
       hanging off the side: */}
       <div
-        style={{ width, height, maxHeight: "100%", maxWidth: "100%" }}
+        style={{
+          width: "90%",
+          "aspect-ratio": "16 / 9",
+          maxWidth: "40em",
+        }}
         className="d-flex justify-content-center align-items-center"
       >
         <div
-          style={frameStyle}
           className="h-100 w-100 d-flex flex-column justify-content-around align-items-center"
+          style={{ position: "relative" }}
         >
           {loaded ? null : <Spinner />}
           <iframe
             className="h-100 w-100"
-            hidden={!loaded}
+            // hidden={!loaded}
             src={src}
             onLoad={() => setLoaded(true)}
             title="YouTube video player"
--- src/components/spinner.js
+++ src/components/spinner.js
@@ -4,7 +4,12 @@ import BaseSpinner from "react-bootstrap/Spinner";
 import styles from "../scss/spinner.module.scss";
 
 const Spinner = () => (
-  <BaseSpinner className={styles.spinner} animation="border" role="status">
+  <BaseSpinner
+    className={styles.spinner}
+    style={{ position: "absolute", top: "48%", left: "48%" }}
+    animation="border"
+    role="status"
+  >
     <span className="visually-hidden">Loading...</span>
   </BaseSpinner>
 );
